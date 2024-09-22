6753761b2990cad923f8ab425754bd744ab8ee6d
Mauricio Alvarez <mauricio.alvarez@utec.edu.pe>
2024-09-21 21:33:45 -0500

    First Version 1.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+// import Vue from 'vue';
+// import Component from 'vue-class-component';
+
 import Header from '../components/Header.vue';
 import Hero from '../components/Hero.vue';
 import WhoWeAre from '../components/WhoWeAre.vue';
@@ -9,6 +12,19 @@ import Clients from '../components/Clients.vue';
 import ContactUs from '../components/ContactUs.vue';
 // import Testimonials from '../components/Testimonials.vue';
 import Footer from '../components/Footer.vue';
+// @Component({
+//   components: {
+//     Header,
+//     Hero,
+//     WhoWeAre,
+//     Services,
+//     Data,
+//     Clients,
+//     ContactUs,
+//     Footer
+//   }
+// })
+// export default class Home extends Vue {}
 </script>
 
 <template>
